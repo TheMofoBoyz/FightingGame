6616c846f2ab1a51d37b1b7ed4b480ad29738103
Bora Oksal <boraoxal@gmail.com>
2019-10-17 17:16:53 +0200

    Rough jump mechanic

--- Assets/BoraScripts/PlayerInput.cs
+++ Assets/BoraScripts/PlayerInput.cs
@@ -20,9 +20,11 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        translation = Input.GetAxis(_cHorizontal) * moveSpeed;
-
-        translation *= Time.deltaTime;
+        translation = Input.GetAxis(_cHorizontal) * Time.deltaTime * moveSpeed;
         transform.position += new Vector3(translation, 0, 0);
+
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, 0f, 1.5f);
+        transform.position = clampedPosition;
     }
 }
---
+++ Assets/BoraScripts/PlayerJump.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    private string _jumpButton;
+    [SerializeField]
+    float jumpForce;
+
+    private bool isGrounded;
+    private float translation;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        translation = Input.GetAxis(_jumpButton) * jumpForce;
+
+        translation *= Time.deltaTime;
+        transform.position += new Vector3(0, translation, 0);
+    }
+}
