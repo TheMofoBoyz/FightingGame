1c64353bcd75e9bf482b581fa68adbc09fbb1932
samjerry <jerry.sam@ziggo.nl>
2019-10-23 12:55:19 +0200

    Replaced old Attack script with a new (working one)

--- Assets/Scripts/Fighting/Attack.cs
+++ Assets/Scripts/Fighting/Attack.cs
@@ -2,11 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Fighting.ComboManager.Attack
+public class Attack : MonoBehaviour
 {
-    public class Attack : MonoBehaviour
+    public Collider[] attackHitboxes;
+
+    private void Update()
     {
-        public string anim_name;
-        public float length;
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            LaunchAttack(attackHitboxes[0]);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            LaunchAttack(attackHitboxes[1]);
+        }
+    }
+
+    private void LaunchAttack(Collider col)
+    {
+        Collider[] cols = Physics.OverlapBox(col.bounds.center,
+                                             col.bounds.extents,
+                                             col.transform.rotation,
+                                             LayerMask.GetMask("Hitbox"));
+        foreach (Collider c in cols)
+        {
+            if (c.transform.parent.parent == transform)
+            {
+                continue;
+            }
+
+                Debug.Log(c.name);
+
+                float damage = 0;
+
+                switch (c.name)
+                {
+                    case "Head":
+                    damage = 30;
+                    break;
+
+                    case "Torso":
+                    damage = 15;
+                    break;
+
+                default:
+                    Debug.Log("The Collider "+ c.name + " Did not match with switch cases. be sure the name is correct");
+                    break;
+                }
+        }
     }
 }
